73a3319adb5976122663b986f4f5f62570ffbad7
myspace20 <balljoe680@gmail.com>
2022-03-26 11:59:55 -0500

    finishing touches

--- map.js
+++ map.js
@@ -12,23 +12,38 @@ form.addEventListener('submit', e => {
     
     e.preventDefault();
     const inputValue = input.value.trim();
-    console.log(inputValue);
     form.reset();
+    
+    
+    const url = `https://api.ipdata.co/${inputValue}?api-key=13f3930663d9d8ee6056709b4a53c80327eae6a11696900923c941f2`
+    
+    if(/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(inputValue)){
+        fetch(url).then(response =>{
+            return response.json()
+        }).then(data =>{
+            
+            ip.innerText = data.ip;
+            locat.innerText = data.city;
+            timezone.innerText = data.time_zone.abbr;
+            isp.innerText = data.asn.name;
+
+            map.panTo(new L.LatLng(data.latitude, data.longitude));
+            marker.setLatLng([data.latitude, data.longitude]).update()
+        })
+    }else{
+        alert('Try a different address, invalid address entered')
+    }
+    
 
 });
 
 const selfInfo =  async  ()=>{
     const response = await fetch('https://api.ipdata.co/?api-key=13f3930663d9d8ee6056709b4a53c80327eae6a11696900923c941f2');
     const data = await response.json();
-    console.log(data);
     return data;
-    
-    
-
 }
 
 selfInfo().then(data =>{
-    console.log(data)
     ip.innerText = data.ip;
     locat.innerText = data.city;
     timezone.innerText = data.time_zone.abbr;
@@ -36,7 +51,22 @@ selfInfo().then(data =>{
 
      
 
-     var map = L.map('map').setView([data.longitude, data.latitude], 13);
+    map.panTo(new L.LatLng(data.latitude, data.longitude));
+    marker.setLatLng([data.latitude, data.longitude]).update()
+     
+});
+
+
+
+
+
+
+
+
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+let marker = L.marker([51.5, -0.09]).addTo(map);
+
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibXlzcGFjZTIwIiwiYSI6ImNrd3I5Y2RrNzBlbzAyb285OWc3ZmNwaWsifQ.u4AVhhvOFCyxTKBL9PAiZQ', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -45,25 +75,5 @@ id: 'mapbox/streets-v11',
 tileSize: 512,
 zoomOffset: -1,
 accessToken: 'your.mapbox.access.token'
+
 }).addTo(map);
-     
-});
-
-
-
-
-
-
-
-
-
-// var map = L.map('map').setView([51.505, -0.09], 13);
-
-// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibXlzcGFjZTIwIiwiYSI6ImNrd3I5Y2RrNzBlbzAyb285OWc3ZmNwaWsifQ.u4AVhhvOFCyxTKBL9PAiZQ', {
-// attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-// maxZoom: 18,
-// id: 'mapbox/streets-v11',
-// tileSize: 512,
-// zoomOffset: -1,
-// accessToken: 'your.mapbox.access.token'
-// }).addTo(map);
